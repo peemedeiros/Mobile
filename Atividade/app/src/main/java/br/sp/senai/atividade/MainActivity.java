c497a653a4f11706fd00c34cb778ade31ebd7947
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-22 17:02:00 -0300

    finalizando atividade

--- Atividade/app/src/main/java/br/sp/senai/atividade/MainActivity.java
+++ Atividade/app/src/main/java/br/sp/senai/atividade/MainActivity.java
@@ -1,13 +1,28 @@
 package br.sp.senai.atividade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activity2 = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(activity2);
+            }
+        });
     }
 }
